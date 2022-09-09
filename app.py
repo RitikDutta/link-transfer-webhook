5c50a39f456a962db91d38acef08c5fba4c92598
ritik dutta <ritikduttagd@gmail.com>
2022-09-09 13:05:40 +0530

    added webhook request

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
- 
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        print("Data received from Webhook is: ", request.json)
+        return "Webhook received!"
+        os.system("xdg-open http://google.com")
+        webbrowser.open_new('xdg-open http://google.com')
+app.run(host='0.0.0.0', port=8000) 

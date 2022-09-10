d708a2afdac2f3914392a21e1dba87f618eaf9ed
ritik dutta <ritikduttagd@gmail.com>
2022-09-10 14:00:15 +0530

    added xgd open for link

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ app = Flask(__name__)
 def webhook():
     if request.method == 'POST':
         print("Data received from Webhook is: ", request.json)
+        url = request.json['message']
+        url = url.replace("'", "")
+        command = "xdg-open {}".format(url)
+        print("URL = ", command)
+        os.system(command)
         return "Webhook received!"
-        os.system("xdg-open http://google.com")
-        webbrowser.open_new('xdg-open http://google.com')
 app.run(host='0.0.0.0', port=8000) 
